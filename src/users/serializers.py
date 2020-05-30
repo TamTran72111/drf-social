9104d4babe36bfed760d1ba5351e6d968b3a0da0
TamTran72111 <tam.tran72111@gmail.com>
2020-05-30 16:34:20 -0400

    Update following info in user serializer

--- src/users/serializers.py
+++ src/users/serializers.py
@@ -5,9 +5,11 @@ from .models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
+    is_following = serializers.SerializerMethodField()
+    followers = serializers.SerializerMethodField()
     class Meta:
         model = User
-        fields = ['username', 'posts']
+        fields = ['username', 'is_following', 'followers', 'posts']
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -16,6 +18,14 @@ class UserSerializer(serializers.ModelSerializer):
             read_only=True,
             context=self.context
         )
-        print("--------------------------")
-        print(self.context.keys())
-        print("--------------------------")
+
+    def get_is_following(self, user):
+        current_user = self.context['request'].user
+        return bool(
+            current_user and 
+            current_user.is_authenticated and 
+            current_user.is_following(user)
+        )
+
+    def get_followers(self, user):
+        return user.followers.count()
